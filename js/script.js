8dd90abac0cc3a52753dd455556e3508fdf8c79e
arthur_rabell <lontra15951@gmail.com>
2023-06-29 16:05:12 +0000

    update 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 document.querySelector('#AtividadeADD').addEventListener("click", FunctionADD);
 
 function FunctionADD(){
-    let AtividadeValue = document.querySelector('#Atividade').value; 
+    /* Pega o valor do input */
+    let AtividadeValue = document.querySelector('#Atividade'); 
 
-    let AtividadeAdcionada = document.createElement("P");
-    let conteudoInserido = document.createTextNode(AtividadeValue);
-    AtividadeAdcionada.appendChild(conteudoInserido)
+    /* Criar as tags */
+    let AtividadeTagDiv = document.createElement("Div");
+    let AtividadeTagP = document.createElement("p");
+    let AtividadeTagCheckBox = document.createElement("input");
+    AtividadeTagCheckBox.setAttribute("type", "checkbox")
 
-    let main = document.querySelector("section");
-    document.body.insertBefore(AtividadeAdcionada, main);
+    AtividadeTagDiv.appendChild(AtividadeTagP);
+    AtividadeTagDiv.appendChild(AtividadeTagCheckBox);
+
+    let AtividadeConteudo = document.createTextNode(AtividadeValue.value);
+    AtividadeTagP.appendChild(AtividadeConteudo);
+    AtividadeTagDiv.classList.add("Atividade");
+
+    let Gambiarra = document.querySelector("#Gambiarra");
+    document.body.insertBefore(AtividadeTagDiv, Gambiarra);
+
+    AtividadeValue.value = "";
 }
